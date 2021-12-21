1cfbe1f0e4889828d907728bde98834337fd45dd
Koshi Ishide <koshi@DESKTOP-TCEGJDK.localdomain>
2021-12-21 21:37:03 +0900

    init

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask,jsonify
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return jsonify({'message':'Hello world'})
-if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=8888, debug=True)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return 'Hello, World!', 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- test_app.py
+++ test_app.py
@@ -1,17 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import unittest
 import app
-
+import json
 
 class TestFlaskHello(unittest.TestCase):
 
     def setUp(self):
-        self.app = flaskhello.app.test_client()
+        self.app = app.app.test_client()
 
     def test_get(self):
         response = self.app.get('/')
         assert response.status_code == 200
-        assert response.data == 'Hello, World!'
-
+        assert response.data.decode() == 'Hello, World!'
 
 if __name__ == '__main__':
     unittest.main()
