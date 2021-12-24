851b7ff32d14c6c3af5799579c93c18741832ed4
koshiishide <koshiishide>
2021-12-24 17:44:06 +0900

    test

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Let's chanbara!', 200
+    return 'Lets chanbara!', 200
 
 if __name__ == '__main__':
     app.run(debug=True)
